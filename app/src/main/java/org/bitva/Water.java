da332e57238ac0217ef5b5158f36ff22b258a711
Buster Daemon <bugsbunnygdi@pm.me>
2024-07-26 15:41:37 +0300

    Добавлена игра "отгадайка" #6

---
+++ app/src/main/java/org/bitva/Water.java
@@ -0,0 +1,74 @@
+package org.bitva;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Water {
+    private int randX;
+    private int randY;
+    private int tries = 0;
+    private boolean xSolved = false;
+    private boolean ySolved = false;
+
+    public Water() {
+        Random rng = new Random();
+        randX = rng.nextInt(101);
+        randY = rng.nextInt(101);
+    }
+
+    public void Start(Player p) {
+        System.out.println("""
+                Сейчас мы сыграем в игру...
+                
+                Твоя задача - отгадать координаты с местонахождением воды.
+                За правильный ответ ты получишь очки, но помни, что с каждой
+                ошибкой твои баллы уменьшаются...
+                
+                Удачи.""");
+        GuessX(p);
+        System.out.println("Ты сделал " + this.tries + " попыток.");
+    }
+
+    private void GuessX(Player p) {
+        System.out.println("Сейчас тебе придётся отгадать координату Х");
+        while (!this.xSolved) {
+            this.xSolved = Guesser(this.xSolved, this.randX);
+        }
+        System.out.println("Сейчас тебе придётся отгадать координату Y");
+        while (!this.ySolved) {
+            this.ySolved = Guesser(this.ySolved, this.randY);
+        }
+    }
+
+    private boolean Guesser(boolean indicator, int target) {
+        int resp;
+        Scanner inp = new Scanner(System.in);
+        while (inp.hasNext() && !indicator) {
+            if (inp.hasNextInt()) {
+                resp = inp.nextInt();
+                if (isThisIt(resp, target)) {
+                    return true;
+                }
+            } else {
+                System.out.println("Это должно быть число!");
+                inp.next();
+            }
+        }
+        return false;
+    }
+
+    private boolean isThisIt(int guesser, int target) {
+        if (guesser < target) {
+            System.out.println("Маловато будет...");
+            this.tries = this.tries + 1;
+            return false;
+        }
+        if (guesser > target) {
+            System.out.println("Что-то ты перегнул, чёрный брат...");
+            this.tries = this.tries + 1;
+            return false;
+        }
+        System.out.println("ВО, то что ты искал.");
+        return true;
+    }
+}
